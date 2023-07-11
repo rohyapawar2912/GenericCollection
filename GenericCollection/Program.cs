57f1a75b5e9bcdfd66de6176766d7b0e47b235f0
“rohyapawar2912” <rohitpawar8530@gmail.com>
2023-07-11 16:54:27 +0530

    [Rohit]Add: // add all files

---
+++ GenericCollection/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericCollection
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Contact> addressBook = new List<Contact>();
+
+            while (true)
+            {
+                Console.WriteLine("Address Book");
+                Console.WriteLine("1. Add Contact");
+                Console.WriteLine("2. Display All Contacts");
+                Console.WriteLine("3. Exit");
+                Console.Write("Enter your choice: ");
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        Contact contact = CreateContact();
+                        addressBook.Add(contact);
+                        Console.WriteLine("Contact added successfully!");
+                        Console.WriteLine();
+                        break;
+                    case "2":
+                        DisplayContacts(addressBook);
+                        break;
+                    case "3":
+                        Console.WriteLine("Exiting Address Book. Goodbye!");
+                        return;
+                    default:
+                        Console.WriteLine("Invalid choice. Please try again.");
+                        break;
+                }
+            }
+        }
+
+        static Contact CreateContact()
+        {
+            Console.WriteLine("Enter Contact Details:");
+
+            Console.Write("First Name: ");
+            string firstName = Console.ReadLine();
+
+            Console.Write("Last Name: ");
+            string lastName = Console.ReadLine();
+
+            Console.Write("Address: ");
+            string address = Console.ReadLine();
+
+            Console.Write("City: ");
+            string city = Console.ReadLine();
+
+            Console.Write("State: ");
+            string state = Console.ReadLine();
+
+            Console.Write("Zip: ");
+            string zip = Console.ReadLine();
+
+            Console.Write("Phone Number: ");
+            string phoneNumber = Console.ReadLine();
+
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+
+            Contact contact = new Contact(firstName, lastName, address, city, state, zip, phoneNumber, email);
+            return contact;
+        }
+
+        static void DisplayContacts(List<Contact> addressBook)
+        {
+            Console.WriteLine("Contacts:");
+
+            foreach (Contact contact in addressBook)
+            {
+                Console.WriteLine($"First Name: {contact.FirstName}");
+                Console.WriteLine($"Last Name: {contact.LastName}");
+                Console.WriteLine($"Address: {contact.Address}");
+                Console.WriteLine($"City: {contact.City}");
+                Console.WriteLine($"State: {contact.State}");
+                Console.WriteLine($"Zip: {contact.Zip}");
+                Console.WriteLine($"Phone Number: {contact.PhoneNumber}");
+                Console.WriteLine($"Email: {contact.Email}");
+                Console.WriteLine();
+            }
+        }
+    }
+}
